ab90bcc83bcbfd0430fc48d005db0d8499590304
Vitor dos Santos Gomes <54445515+santosgv@users.noreply.github.com>
2021-04-07 11:22:41 -0300

    ultimos ajustes

--- inpython/main.py
+++ inpython/main.py
@@ -21,16 +21,20 @@ class Validadores():
         if veiculo != True:
             self.Stsvaiculo['text'] = 'Veiculo Cadastrado'
 
-    def movimentacao(self):
-        pass
-
     def Regentrada(self):
         entrada = database.Banco()
         veiculo=int(self.entent.get())
-        entrada.Regientrada(veiculo)
+        vaga=int(self.self.entrpraca.get())
+        entrada.Regientrada(veiculo,vaga)
         if veiculo !=True:
             self.lbentrada['text']="Entrada registrada"
 
+    def Regsaida(self):
+        saida=database.Banco()
+        idsaida=int(self.entsaida.get())
+        saida.Regisaida(idsaida)
+        if saida !=True:
+            self.lbsaida['text']="SAIDA REGISTRADA"
 
 class Aplicacao(Validadores):
     def __init__(self):
@@ -122,13 +126,6 @@ class Aplicacao(Validadores):
                 self.grid.insert("", END, values=(linhas[0],linhas[1],linhas[2],linhas[3]))
         Gredviw()
 
-    def Imovimentacao(self):
-        iMovie = Toplevel(self.principal, bg='white')
-        iMovie.geometry('250x184')
-        iMovie.title('Movimentacao de Praca')
-        iMovie.resizable(False, False)
-        Button(iMovie, text='Movimentacao de Praca', command=self.movimentacao).place(relx=0.01, rely=0.10)
-
     def Ientrada(self):
         ientrada = Toplevel(self.principal, bg='white')
         ientrada.geometry('250x184')
@@ -137,6 +134,11 @@ class Aplicacao(Validadores):
         Label(ientrada, text='Codigo veiculo', bg='white').place(relx=0.30, rely=0.01)
         self.entent = Entry(ientrada)
         self.entent.place(relx=0.20, rely=0.15)
+
+        Label(ientrada, text='Codigo da praca', bg='white').place(relx=0.01, rely=0.30)
+        self.entrpraca=Entry(ientrada)
+        self.entrpraca.place(relx=0.45, rely=0.30)
+
         Button(ientrada, text='Exibir veiculos', command=self.Ishowvec).place(relx=0.30, rely=0.60)
         Button(ientrada, text='registrar',command=self.Regentrada).place(relx=0.35, rely=0.80)
         self.lbentrada=Label(ientrada,text='',bg='white')
@@ -166,7 +168,8 @@ class Aplicacao(Validadores):
         def Gredviw():
             self.gridocupado.delete(*self.gridocupado.get_children())
             banco = database.Banco()
-            resultado=banco.cur.execute(f'''select A.ID,A.DATAINICIO,B.PLACA,B.COR,B.TIPO from movimentacao A JOIN VEICULO B ON A.VEICULO=B.ID''')
+            resultado=banco.cur.execute(f'''select A.ID,A.DATAINICIO,B.PLACA,B.COR,B.TIPO from movimentacao A JOIN VEICULO B ON A.VEICULO=B.ID where OCUPADO='S'
+            ''')
 
             for linhas in resultado:
                 self.gridocupado.insert("", END, values=(linhas[0],linhas[1],linhas[2],linhas[3],linhas[4]))
@@ -177,16 +180,49 @@ class Aplicacao(Validadores):
         isaida.geometry('250x184')
         isaida.title('Saida de veiculo')
         isaida.resizable(False, False)
-        Label(isaida, text='Codigo veiculo', bg='white').place(relx=0.30, rely=0.01)
-        self.entent = Entry(isaida)
-        self.entent.place(relx=0.20, rely=0.15)
+        Label(isaida, text='Codigo saida', bg='white').place(relx=0.30, rely=0.01)
+        self.entsaida = Entry(isaida)
+        self.entsaida.place(relx=0.20, rely=0.15)
         Button(isaida, text='Ocupacao',command=self.Iocupacao).place(relx=0.30, rely=0.60)
-        Button(isaida, text='Dar saida').place(relx=0.30, rely=0.80)
+        Button(isaida, text='Dar saida',command=self.Regsaida).place(relx=0.30, rely=0.80)
         self.lbsaida=Label(isaida,text='',bg='white')
         self.lbsaida.place(relx=0.25, rely=0.40)
 
+    def Imovimen(self):
+        Imovimentacao = Toplevel(self.principal, bg='white')
+        Imovimentacao.geometry('450x250')
+        Imovimentacao.title('Movimentacao')
+        Imovimentacao.resizable(True, True)
+        self.gridmovi = ttk.Treeview(Imovimentacao, columns=('col1', 'col2', 'col3', 'col4','col5','col6'), show='headings')
+        self.gridmovi.place(relx=0.01, rely=0.1, relwidth=0.95, relheight=0.95)
+        self.gridmovi.column('#0', width=0, minwidth=0)
+        self.gridmovi.column('col1', minwidth=0, width=20)
+        self.gridmovi.column('col2', minwidth=0, width=100)
+        self.gridmovi.column('col3', minwidth=0, width=100)
+        self.gridmovi.column('col4', minwidth=0, width=100)
+        self.gridmovi.column('col5', minwidth=0, width=20)
+        self.gridmovi.column('col6', minwidth=0, width=100)
+        self.gridmovi.heading('#0', text='')
+        self.gridmovi.heading('#1', text='ID')
+        self.gridmovi.heading('#2', text='ENTRADA')
+        self.gridmovi.heading('#3', text='PLACA')
+        self.gridmovi.heading('#4', text='COR')
+        self.gridmovi.heading('#5', text='T')
+        self.gridmovi.heading('#6', text='SAIDA')
+        self.gridmovi.pack()
+
+        def Gredviw():
+            self.gridmovi.delete(*self.gridmovi.get_children())
+            banco = database.Banco()
+            resultado=banco.cur.execute(f'''select A.ID,A.DATAINICIO,B.PLACA,B.COR,B.TIPO,a.DATAFINAL from movimentacao A JOIN VEICULO B ON A.VEICULO=B.ID
+            ''')
+
+            for linhas in resultado:
+                self.gridmovi.insert("", END, values=(linhas[0],linhas[1],linhas[2],linhas[3],linhas[4],linhas[5]))
+        Gredviw()
+
     def LbandButtons(self):
-        Button(self.principal, text='Movimentacao', fg='White', bg='#836FFF', command=self.Imovimentacao).place(
+        Button(self.principal, text='Movimentacao', fg='White', bg='#836FFF', command=self.Imovimen).place(
             relx=0.05, rely=0.10)
         Button(self.principal, text='Registrar entrada', fg='white', bg='#836FFF', command=self.Ientrada).place(
             relx=0.42, rely=0.10)
